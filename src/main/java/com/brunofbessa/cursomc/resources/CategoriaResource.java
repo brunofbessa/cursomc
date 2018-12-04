bfd7e657e0a866e863cd57828be686638fc81eac
Bruno F. Bessa <brunofbessa@gmail.com>
2018-12-03 22:51:12 -0200

    Ímplementação da classe de domínio Categorias

--- src/main/java/com/brunofbessa/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/brunofbessa/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.brunofbessa.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brunofbessa.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,21 @@ public class CategoriaResource {
 	
 	// Abaixo um teste simples para verificar se o resource está respondendo adequadamente ao HTTP.
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		// Criar uma lista de objetos categoria
+		// Para tanto, deve-se importar a lib que implementa listas
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		// Método retornará a lista
+		return lista;
 	}
 
 }
