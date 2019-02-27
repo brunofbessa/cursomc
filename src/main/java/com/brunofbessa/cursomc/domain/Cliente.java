da2d1b6f6e96316568d1602e8f73f297aa024b03
Bruno F. Bessa <brunofbessa@gmail.com>
2019-02-26 23:33:34 -0300

    Implementação de endpoint de cliente.

--- src/main/java/com/brunofbessa/cursomc/domain/Cliente.java
+++ src/main/java/com/brunofbessa/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.brunofbessa.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable{
@@ -29,6 +30,7 @@ public class Cliente implements Serializable{
 	// Armazenar internamente o número definido pela implementação de TipoCliente, mas expor o tipo completo
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
