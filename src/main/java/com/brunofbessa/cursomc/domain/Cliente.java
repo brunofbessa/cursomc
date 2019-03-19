5025b753f78bfe07bb73cd4fbbb2829ac30d579e
Bruno F. Bessa <brunofbessa@gmail.com>
2019-03-18 22:56:50 -0300

    Associacoes Pedido Cliente e Pagamento

--- src/main/java/com/brunofbessa/cursomc/domain/Cliente.java
+++ src/main/java/com/brunofbessa/cursomc/domain/Cliente.java
@@ -39,6 +39,18 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+	
 	public Cliente() {
 		
 	}
@@ -132,7 +144,6 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
 	
 
 }
